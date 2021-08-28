47aa3ca1a36a5691ccfafd6c20cfcb0a2d34516d
Ademir <ademir.m.junior@gmail.com>
2021-08-27 22:50:42 -0300

    Develop buttons action

--- app/src/main/java/br/edu/utfpr/supportdifferentlanguages/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/supportdifferentlanguages/MainActivity.java
@@ -3,12 +3,42 @@ package br.edu.utfpr.supportdifferentlanguages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextFirstName, editTextLastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+
+        setTitle(getString(R.string.new_record));
+
+        Toast.makeText(this,
+                R.string.welcome_message,
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void submit(View view) {
+        String firstName = editTextFirstName.getText().toString();
+        String lastName = editTextLastName.getText().toString();
+
+        Toast.makeText(this,
+                getString(R.string.full_name) + " " + firstName + " " + lastName,
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void clear(View view) {
+        editTextFirstName.setText(null);
+        editTextLastName.setText(null);
+
+        editTextFirstName.requestFocus();
     }
 }
